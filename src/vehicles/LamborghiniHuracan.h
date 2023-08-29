73b6b486ebbdcd20207ff084ea1fe38ecc17f383
Ralf Engels <ralf-engels@gmx.de>
2023-08-29 09:15:37 +0200

    updates for huracan

--- src/vehicles/LamborghiniHuracan.h
+++ src/vehicles/LamborghiniHuracan.h
@@ -1,103 +1,94 @@
 #include <Arduino.h>
 
-// Vehicle specific settings for sound, lights, ESC, transmission. This is a vehicle specific file.
-// Should emulate the sound of a Lamborghini Huracan Evo
+// Lamborghini Huracan Evo
+// note: The sound is really different depending on your position
+// around the car (e.g. exaust) and the mode (e.g. corsa mode)
+//
+// So this is kind of generic
 
 // Sound files (22'050 Hz, 8 bit PCM recommended) -----------------------------------------------------------------------
 // Choose the start sound (uncomment the one you want) --------
-volatile int startVolumePercentage = 120; // Adjust the start volume (usually = 80%)
-#include "sounds/LaFerrariStart.h" // Ferrari LaFerrari, V12
+const int32_t startVolumePercentage = 100;
+#include "sounds/HuracanStart4.h"
 
 // Choose the motor idle sound (uncomment the one you want) --------
-volatile int idleVolumePercentage = 64; // Adjust the idle volume (usually = 80%, more also working, depending on sound, 40 - 48% if additional diesel knock sound is used)
-volatile int engineIdleVolumePercentage = 60; // the engine volume will be throttle dependent (usually = 40%, never more than 100%!)
-volatile int fullThrottleVolumePercentage = 130; // Volume Percentage while full throttle (for rev sound as well)
-#include "sounds/LaFerrariIdle.h" // Jaguar XJS V12
-//#include "sounds/idleDummy.h" // Dummy
+const int32_t idleVolumePercentage = 100;
+const int32_t engineIdleVolumePercentage = 5;
+const int32_t fullThrottleVolumePercentage = 100;
+#include "sounds/HuracanIdle4.h"
 
 // Choose the motor revving sound (uncomment the one you want) --------
-#define REV_SOUND // uncomment this, if you want to use the separate, optional rev sound
-volatile int revVolumePercentage = 96; // Adjust the idle volume (usually = 80%, more also working, depending on sound)
-volatile int engineRevVolumePercentage = 60; // the engine volume will be throttle dependent (usually = 40%, never more than 100%!)
-volatile const uint16_t revSwitchPoint = 50; // The rev sound is played instead of the idle sound above this point
-volatile const uint16_t idleEndPoint = 300; // above this point, we have 100% rev and 0% idle sound volume (usually 500, min. 50 more than revSwitchPoint)
-volatile const uint16_t idleVolumeProportionPercentage = 100; // The idle sound volume proportion (rest is rev proportion) below "revSwitchPoint" (about 90 - 100%, never more than 100)
+// #define REV_SOUND // uncomment this, if you want to use the separate, optional rev sound
+const int32_t revVolumePercentage = 100; // Adjust the idle volume (usually = 100%, more also working, depending on sound)
+const int32_t engineRevVolumePercentage = 60; // the engine volume will be throttle dependent (usually = 40%, never more than 100%!)
+const uint16_t revSwitchPoint = 50; // The rev sound is played instead of the idle sound above this point
+const uint16_t idleEndPoint = 300; // above this point, we have 100% rev and 0% idle sound volume (usually 500, min. 50 more than revSwitchPoint)
+const uint16_t idleVolumeProportionPercentage = 100; // The idle sound volume proportion (rest is rev proportion) below "revSwitchPoint" (about 90 - 100%, never more than 100)
 #ifdef REV_SOUND
-#include "sounds/LaFerrariRev.h" // Ferrari LaFerrari, V12
+#include "sounds/HuracanRev3.h"
 #endif
 
 // Choose the jake brake sound (uncomment the one you want) --------
-//#define JAKE_BRAKE_SOUND // uncomment this, if you want to use the jake brake sound
-volatile int jakeBrakeVolumePercentage = 140; // Adjust the max. volume (usually = 150%)
-volatile int jakeBrakeIdleVolumePercentage = 0; // Adjust the min. volume (usually = 80%)
-volatile int jakeBrakeMinRpm = 200; // Adjust the min. RPM for the jake brake (around 100)
+//#define JAKE_BRAKE_SOUND
+const int32_t jakeBrakeVolumePercentage = 0;
+const int32_t jakeBrakeIdleVolumePercentage = 0;
+const int32_t jakeBrakeMinRpm = 200;
 #ifdef JAKE_BRAKE_SOUND
-#include "sounds/JakeBrake.h" // CAT V8 Jake Brake sound
+#include "sounds/JakeBrake.h"
 #endif
 
-// Choose the Diesel (or whatever) ignition "knock" sound (played in the fixed sampling rate interrupt, uncomment the one you want,
-// play around here, the results are amazing, if you hit the right combination with the idle sound!) --------
-volatile int dieselKnockVolumePercentage = 120; // Adjust the Diesel knock volume (usually = 40 - 120%) 140
-volatile int dieselKnockIdleVolumePercentage = 0; // Diesel knock volume while no throttle is applied (usually = 20%)
-volatile int dieselKnockStartPoint = 10; // Volume will raise above this throttle input( usually 0, for "open pipe" exhaust about 250)
-volatile int dieselKnockInterval = 12; // Idle sample length divided by this number (1 - 20, depending on sound files)
-#define V8 // V8 engine (Ford, Scania): pulses 4 and 8 will bel louder, because only 90° gap between exhaust cycles in same manifold
-//#define V2 // V2 engine (Harley): first 2 of 4 pulses will be louder (set dieselKnockInterval to 4)
-volatile int dieselKnockAdaptiveVolumePercentage = 50; // Adjust the Diesel knock volume for the non-first knocks per engine cycle (usually = 50%)
-#define RPM_DEPENDENT_KNOCK // Knock volume also depending on engine RPM *****************
-#ifdef RPM_DEPENDENT_KNOCK // These parameters are for RPM dependent knock mode only:
-uint8_t minKnockVolumePercentage = 5; // percentage, if at knock start RPM (about 5 - 80)
-uint16_t knockStartRpm = 400; // starting @ this RPM (about 50 - 400)
-#endif // ********************************************************************************
-#include "sounds/LaFerrariKnock.h" // Ferrari LaFerrari, V12
+const int32_t dieselKnockVolumePercentage = 0;
+const int32_t dieselKnockIdleVolumePercentage = 0;
+const int32_t dieselKnockStartPoint = 10;
+const int32_t dieselKnockInterval = 12;
+#define V8
+volatile int dieselKnockAdaptiveVolumePercentage = 50;
+// #define RPM_DEPENDENT_KNOCK
+#ifdef RPM_DEPENDENT_KNOCK
+uint8_t minKnockVolumePercentage = 5;
+uint16_t knockStartRpm = 400;
+#endif
+#include "sounds/LaFerrariKnock.h"
 
-// Adjust the additional turbo sound (set "turboVolumePercentage" to "0", if you don't want it) --------
-// Lamborghini doesn't have a turbo or a supercharger
-volatile int turboVolumePercentage = 0; // Adjust the turbo volume (usually = 14%)
-volatile int turboIdleVolumePercentage = 0; // the turbo volume will be engine rpm dependent (usually = 10%)
-#include "sounds/TurboWhistle.h" // Turbo sound, playing in parallel with engine sound!
+// Lamborghini Huracan Evo doesn't have a turbo or a supercharger
+const int32_t turboVolumePercentage = 0;
+const int32_t turboIdleVolumePercentage = 0;
+#include "sounds/TurboWhistle.h"
 
-// Adjust the additional supercharger sound (set "chargerVolumePercentage" to "0", if you don't want it) --------
-volatile int chargerVolumePercentage = 0; // Adjust the supercharger volume (usually = 14%)
-volatile int chargerIdleVolumePercentage = 10; // the supercharger volume will be engine rpm dependent (usually = 10%)
-volatile int chargerStartPoint = 10; // Volume will raise above this point ( usually 10)
-#include "sounds/supercharger.h" // supercharger sound, playing in parallel with engine sound!
+const int32_t chargerVolumePercentage = 0;
+const int32_t chargerIdleVolumePercentage = 10;
+const int32_t chargerStartPoint = 10;
+#include "sounds/supercharger.h"
 
-// Adjust the additional turbo wastegate  / blowoff valve  sound (set "wastegateVolumePercentage" to "0", if you don't want it)--------
-volatile int wastegateVolumePercentage = 18; // Adjust the wastegate volume (usually = 14%, up to 50%)
-volatile int wastegateIdleVolumePercentage = 1; // Wastegate sound is played, after rapid throttle drop with engaged clutch
+const int32_t wastegateVolumePercentage = 0;
+const int32_t wastegateIdleVolumePercentage = 1;
 #include "sounds/WastegateDummy.h"
-//#include "sounds/UnimogU1000TurboWastegate.h"
-//#include "sounds/ScaniaR730V8TurboWastegate.h"
 
 // Adjust the additional cooling fan sound (set "fanVolumePercentage" to "0", if you don't want it) --------
-volatile int fanVolumePercentage = 0; // Adjust the fan volume (50% for Tatra 813, else 0%)
-volatile int fanIdleVolumePercentage = 0; // the fan volume will be engine rpm dependent (usually = 10%)
-volatile int fanStartPoint = 0; // Volume will raise above this point (250 for Tatra 813)
+const int32_t fanVolumePercentage = 0; // Adjust the fan volume (50% for Tatra 813, else 0%)
+const int32_t fanIdleVolumePercentage = 0; // the fan volume will be engine rpm dependent (usually = 10%)
+const int32_t fanStartPoint = 0; // Volume will raise above this point (250 for Tatra 813)
 #include "sounds/GenericFan.h" // Generic engine cooling fan
 
 // Choose the horn sound (uncomment the one you want) --------
-volatile int hornVolumePercentage = 80; // Adjust the horn volume (usually = 80%)
+const int32_t hornVolumePercentage = 60; // Adjust the horn volume (usually = 100%)
 #include "sounds/CarHorn.h" // A boring car horn
 
 // Choose the siren / additional horn sound (uncomment the one you want) --------
-volatile int sirenVolumePercentage = 80; // Adjust the siren volume (usually = 80%)
+#define NO_SIREN
+const int32_t sirenVolumePercentage = 100; // Adjust the siren volume (usually = 100%)
 #include "sounds/sirenDummy.h" // If you don't want siren sound
 
-// Choose the air brake release sound (uncomment the one you want) --------
-volatile int brakeVolumePercentage = 30; // Adjust the brake volume (usually = 40%)
-#include "sounds/AirBrakeDummy.h" // If you don't want air brake sound
+const int32_t brakeVolumePercentage = 0;
+#include "sounds/AirBrakeDummy.h"
 
-// Choose the parking brake engaging sound (uncomment the one you want) --------
-volatile int parkingBrakeVolumePercentage = 30; // Adjust the brake volume (usually = 40%)
-#include "sounds/ParkingBrakeDummy.h" // If you don't want parking brake sound
-//#include "sounds/ParkingBrake.h" // Parking brake sound
+const int32_t parkingBrakeVolumePercentage = 0;
+#include "sounds/ParkingBrakeDummy.h"
 
-// Choose the gear shifting sound (uncomment the one you want) --------
-volatile int shiftingVolumePercentage = 20; // Adjust the shifting volume (usually = 40%)
-//#include "sounds/AirShiftingDummy.h" // If you don't want pneumatic shifting sound
+volatile int shiftingVolumePercentage = 00;
+#include "sounds/AirShiftingDummy.h" // If you don't want pneumatic shifting sound
 //#include "sounds/AirShifting.h" // Pneumatic shifting sound
-#include "sounds/ClunkingGearShifting.h" // Manual clunking shifting sound
+// #include "sounds/ClunkingGearShifting.h" // Manual clunking shifting sound
 
 // Choose the additional "sound1" (uncomment the one you want) --------
 volatile int sound1VolumePercentage = 100; // Adjust the sound1 volume (usually = 100%)
@@ -107,19 +98,17 @@ volatile int sound1VolumePercentage = 100; // Adjust the sound1 volume (usually 
 //#include "sounds/GlenCanyon.h" // Glen Canyon country song for truckers ;-)
 #include "sounds/door.h" // opening and closing the door
 
-// Choose the reversing beep sound --------
-volatile int reversingVolumePercentage = 0; // Adjust the reversing sound volume (usually = 14%)
-#include "sounds/TruckReversingBeep.h" // 1000Hz peep sound
+volatile int reversingVolumePercentage = 0;
+#include "sounds/TruckReversingBeep.h"
 
 // Choose the indicator / turn signal options --------
-volatile int indicatorVolumePercentage = 10; // Adjust the indicator sound volume (usually = 10%)
-const uint16_t indicatorOn = 300; // The indicator will be switched on above +/- this value, if wheels are turned
-const boolean INDICATOR_DIR = true; // adjust indicator direction with true or false
-#include "sounds/Indicator.h" // "Tick-Tack" sound
+volatile int indicatorVolumePercentage = 10;
+const uint16_t indicatorOn = 300;
+const boolean INDICATOR_DIR = true;
+#include "sounds/Indicator.h"
 
-// Choose the tire squealing sound (uncomment the ones you want) --------
 #define TIRE_SQUEAL
-volatile int tireSquealVolumePercentage = 160; // Adjust the tire squeal volume
+volatile int tireSquealVolumePercentage = 100;
 #include "sounds/TireSqueal2.h"
 
 #define SEPARATE_FULL_BEAM // separate full beam bulb, wired to "rooflight" pin ----
@@ -132,13 +121,13 @@ const boolean doubleFlashBlueLight = true; // double flash blue lights if "true"
 
 // Acceleration & deceleration settings ----------------------------------------------------------------------------------
 const uint8_t escRampTimeFirstGear = 20; // determines, how fast the acceleration and deceleration happens (about 15 - 25, 20 for King Hauler)
-const uint8_t escRampTimeSecondGear = 50; // 50 for King Hauler (this value is always in use for automatic transmission, about 80)
-const uint8_t escRampTimeThirdGear = 75; // 75 for King Hauler
-const uint8_t escBrakeSteps = 30; // determines, how fast the ESC is able to brake down (20 - 30, 30 for King Hauler)
-const uint8_t escAccelerationSteps = 3; // determines, how fast the ESC is able to accelerate (2 - 3, 3 for King Hauler)
+const uint8_t escRampTimeSecondGear = 30; // 50 for King Hauler (this value is always in use for automatic transmission, about 80)
+const uint8_t escRampTimeThirdGear = 40; // 75 for King Hauler
+const uint8_t escBrakeSteps = 50; // determines, how fast the ESC is able to brake down (20 - 30, 30 for King Hauler)
+const uint8_t escAccelerationSteps = 5; // determines, how fast the ESC is able to accelerate (2 - 3, 3 for King Hauler)
 
 // Gearbox parameters ---------------------------------------------------------------------------------------------------
-const boolean automatic = false; // false = linear rpm curve, true = automatic transmission with torque converter is simulated (use it, if you don't have a real shifting transmission)
+const boolean automatic = true; // false = linear rpm curve, true = automatic transmission with torque converter is simulated (use it, if you don't have a real shifting transmission)
 #define NumberOfAutomaticGears 6 // <<------- Select 3, 4 or 6 gears!
 const boolean doubleClutch = true; // do not activate it at the same time as automatic!
 const boolean shiftingAutoThrottle = true; // For Tamiya 3 speed tansmission, throttle is altered for synchronizing, if "true"
@@ -148,9 +137,9 @@ uint16_t clutchEngagingPoint = 100; // CEP. The "clutch" is engaging above this 
 
 // Engine parameters ----------------------------------------------------------------------------------------------------
 // Engine max. RPM in % of idle RPM. About 200% for big Diesels, 390% for fast running motors.
-uint32_t MAX_RPM_PERCENTAGE = 400; // NOTE! Was called TOP_SPEED_MULTIPLIER (TSM) in earlier versions and was a multiplier instead of a percentage!
+uint32_t MAX_RPM_PERCENTAGE = 350; // NOTE! Was called TOP_SPEED_MULTIPLIER (TSM) in earlier versions and was a multiplier instead of a percentage!
 
 // Engine mass simulation
 const int8_t acc = 2; // Acceleration step (2) 1 = slow for locomotive engine, 9 = fast for trophy truck. Always 2 for automatic transmission!
-const int8_t dec = 1; // Deceleration step (1) 1 = slow for locomotive engine, 5 = fast for trophy truck
+const int8_t dec = 10; // Deceleration step (1) 1 = slow for locomotive engine, 5 = fast for trophy truck
 
